f2567b40819fb8dbc294d2cf5661a40faec04dca
DursituK <dursitukedir414@gmail.com>
2026-07-13 10:00:47 +0300

    Add new folder and files

---
+++ Programming Basics/JS/script.js
@@ -0,0 +1,53 @@
+console.log(24 > 3);
+console.log(2 < "12");
+console.log(0 == 2);
+console.log(2.0 === 2);
+console.log(2.0 == "2");
+console.log(2 < "John");
+console.log(2 > "John");
+console.log("2" < "2");
+console.log("2" > "12");
+console.log(1 == 1 || 3 == 2 || 3 == 7);
+console.log(1 == 1 && 2 == 2 && 3 == 7);
+console.log(1 == 1 || 2 == 2 && 3 == 7);
+console.log(1 == true && 0 > true || "31" > "9" || 10 > 5 || !("2" == "two" || 1 == "1"));// Question 2.2
+let x = (1 == true);
+console.log(x);
+
+// Question 2.3
+let a = 10;
+let y = (a > 5) && (a < 15);
+console.log(y);
+
+// Question 2.4
+let b = 5;
+b += 3;
+console.log(b);
+
+// Question 2.5
+let c = 10;
+let d = c++;
+console.log(d);
+
+// Question 2.6
+let e = 1;
+let f = (e !== 2);
+console.log(f);
+
+// Question 2.7
+console.log(+"2" + 2);
+
+// Question 2.8
+console.log(7 % 3);
+
+// Question 2.9
+console.log(2 + true);let num1 = 1;
+let num2 = 2;
+let result = num1 + num2;
+
+console.log(result);let firstName = "Dursitu";
+let lastName = "Kedir";
+
+let fullName = firstName + " " + lastName;
+
+console.log(fullName);
